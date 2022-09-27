df9124383c980c18fe3b70d403b9ecaebd1bdc3c
omenaka102 <93178465+omenaka102@users.noreply.github.com>
2022-09-27 13:54:39 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore maxime porro quas dolorem facilis. Laudantium commodi fuga quis quidem molestias? Cupiditate blanditiis vitae quas debitis eos. Deleniti dignissimos ducimus, inventore corporis quidem eum temporibus optio beatae nostrum sunt quas ab.
+      </p>
+    </section>
+  )
+}
+
+export default About
